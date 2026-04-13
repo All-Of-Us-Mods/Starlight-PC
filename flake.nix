349f3b2b109d0cd3e1596904abe3d2a50a58c2f1
yanpla <yannick@plantenga.com>
2026-04-13 14:27:33 +0000

    tauri deps for flake.nix

--- flake.nix
+++ flake.nix
@@ -24,6 +24,18 @@
           packages = with pkgs; [
             bun
             nodejs_24
+            pkg-config
+            openssl
+            glib
+            gtk3
+            webkitgtk_4_1
+            libsoup_3
+            glib-networking
+            cacert
+            cairo
+            pango
+            gdk-pixbuf
+            atk
             rustc
             cargo
             rustfmt
@@ -31,10 +43,15 @@
           ];
 
           shellHook = ''
+            export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/"
+            export GIO_EXTRA_MODULES="${pkgs.glib-networking}/lib/gio/modules/"
+            export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+
             echo "Bun + Rust dev shell ready"
             bun --version
             node --version
             cargo --version
+            pkg-config --version
           '';
         };
       }
