a54343113c24139d4b3496ea85d547eff842bd0a
yanpla <yannick@plantenga.com>
2026-05-01 23:25:55 +0200

    add package build to nix flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Dev shell with Bun and Rust for Starlight PC";
+  description = "Starlight PC";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -12,8 +12,171 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+
+        pname = "starlight";
+        version = "1.1.1";
+
+        src = lib.cleanSourceWith {
+          src = ./.;
+          filter =
+            path: type:
+            let
+              rel = lib.removePrefix (toString ./. + "/") (toString path);
+              base = baseNameOf path;
+            in
+            !(lib.hasPrefix ".git/" rel)
+            && !(lib.hasPrefix "build/" rel)
+            && !(lib.hasPrefix "node_modules/" rel)
+            && !(lib.hasPrefix "src-tauri/target/" rel)
+            && base != "result";
+        };
+
+        bunDeps = pkgs.stdenvNoCC.mkDerivation {
+          pname = "${pname}-bun-deps";
+          inherit version src;
+
+          nativeBuildInputs = [ pkgs.bun ];
+
+          dontConfigure = true;
+          dontBuild = true;
+
+          installPhase = ''
+            runHook preInstall
+
+            export HOME="$TMPDIR"
+            bun install --frozen-lockfile --ignore-scripts
+
+            mkdir -p "$out"
+            cp -R node_modules "$out/"
+
+            runHook postInstall
+          '';
+
+          outputHashAlgo = "sha256";
+          outputHashMode = "recursive";
+          outputHash = "sha256-81JoWZNK5PYYmsIOWb7KLZyw7P3Y2B3czuye2b5XxGM=";
+        };
+
+        frontend = pkgs.stdenvNoCC.mkDerivation {
+          pname = "${pname}-frontend";
+          inherit version src;
+
+          nativeBuildInputs = [
+            pkgs.bun
+            pkgs.nodejs
+          ];
+
+          env.PUBLIC_API_URL = "https://starlight.allofus.dev";
+
+          configurePhase = ''
+            runHook preConfigure
+
+            export HOME="$TMPDIR"
+            cp -R ${bunDeps}/node_modules ./node_modules
+            chmod -R u+w ./node_modules
+            patchShebangs ./node_modules
+
+            runHook postConfigure
+          '';
+
+          buildPhase = ''
+            runHook preBuild
+
+            bun run build
+
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p "$out"
+            cp -R build "$out/"
+
+            runHook postInstall
+          '';
+        };
       in
       {
+        packages.default = pkgs.stdenv.mkDerivation {
+          inherit pname version src;
+
+          cargoDeps = pkgs.rustPlatform.importCargoLock {
+            lockFile = ./src-tauri/Cargo.lock;
+          };
+          cargoRoot = "src-tauri";
+
+          nativeBuildInputs = with pkgs; [
+            cargo
+            nodejs
+            pkg-config
+            rustPlatform.cargoSetupHook
+            rustc
+            wrapGAppsHook4
+          ];
+
+          buildInputs = with pkgs; [
+            glib-networking
+            librsvg
+            openssl
+            webkitgtk_4_1
+          ];
+
+          env = {
+            OPENSSL_NO_VENDOR = "1";
+          };
+
+          postPatch = ''
+            cp -R ${frontend}/build ./build
+            chmod -R u+w ./build
+          '';
+
+          buildPhase = ''
+            runHook preBuild
+
+            cargo build --manifest-path src-tauri/Cargo.toml --release --locked
+
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+
+            bin="$(find . \( -path '*/target/release/Starlight' -o -path '*/target/release/starlight' \) -type f -perm -0100 | head -n1)"
+            if [ -z "$bin" ]; then
+              echo "could not find built starlight binary" >&2
+              find . -path '*/target/release/*' -maxdepth 4 -type f >&2
+              exit 1
+            fi
+            install -Dm755 "$bin" "$out/bin/starlight"
+            install -Dm644 src-tauri/icons/128x128.png "$out/share/icons/hicolor/128x128/apps/starlight.png"
+            install -Dm644 static/starlight.png "$out/share/pixmaps/starlight.png"
+
+            install -Dm644 /dev/stdin "$out/share/applications/starlight.desktop" <<EOF
+            [Desktop Entry]
+            Type=Application
+            Name=Starlight
+            Comment=Among Us mod manager
+            Exec=starlight
+            Icon=starlight
+            Categories=Game;
+            Terminal=false
+            StartupWMClass=Starlight
+            EOF
+
+            runHook postInstall
+          '';
+
+          meta = {
+            description = "Among Us mod manager";
+            homepage = "https://github.com/All-Of-Us-Mods/Starlight-PC";
+            license = lib.licenses.gpl3Only;
+            mainProgram = "starlight";
+            platforms = lib.platforms.linux;
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             pkg-config
