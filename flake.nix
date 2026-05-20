dd0d28795ecdf0e99a371305641f025e8731d7cc
yanpla <yannick@plantenga.com>
2026-05-20 21:06:37 +0200

    stuff

--- flake.nix
+++ flake.nix
@@ -42,17 +42,59 @@
           xorg.libXi
           xorg.libxcb
         ];
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        starlight = rustPlatform.buildRustPackage {
+          pname = "starlight";
+          version = (fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "collections-0.1.0" = "sha256-+9t67JeCwlI01nuWp1jONiyl/4u1O7ruzPYEj1+Jp2Q=";
+              "gpui-component-0.5.2" = "sha256-gCXOFwEpiaZrJfNhO3yO37qr6qX5rs+9/8ff2TqZXAQ=";
+              "naga-29.0.3" = "sha256-jwPdrd2XLvK5ddEutR/39OLMh2JU3UXNWIcJKCndh+U=";
+              "zed-font-kit-0.14.1-zed" = "sha256-KXygi0olNQi5yM8eaJVykNDtbPMDjT+cWPBF8UrtXR4=";
+              "zed-reqwest-0.12.15-zed" = "sha256-p4SiUrOrbTlk/3bBrzN/mq/t+1Gzy2ot4nso6w6S+F8=";
+              "zed-scap-0.0.8-zed" = "sha256-BihiQHlal/eRsktyf0GI3aSWsUCW7WcICMsC2Xvb7kw=";
+            };
+          };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            makeWrapper
+          ];
+
+          meta = with pkgs.lib; {
+            description = "Among Us mod manager";
+            license = licenses.gpl3Only;
+            platforms = platforms.linux;
+            mainProgram = "Starlight";
+          };
+        };
       in
       {
+        packages = {
+          default = starlight;
+          starlight = starlight;
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${starlight}/bin/Starlight";
+        };
+
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs =
-            [ toolchain ]
-            ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.pkg-config ];
+          nativeBuildInputs = [ toolchain ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.pkg-config ];
 
           buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux linuxLibs;
 
-          LD_LIBRARY_PATH =
-            pkgs.lib.optionalString pkgs.stdenv.isLinux (pkgs.lib.makeLibraryPath linuxLibs);
+          LD_LIBRARY_PATH = pkgs.lib.optionalString pkgs.stdenv.isLinux (pkgs.lib.makeLibraryPath linuxLibs);
         };
       }
     );
