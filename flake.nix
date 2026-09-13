47a5a1d6e5de1b921140c3f876c50a336434511c
yanpla <me@yanpla.nl>
2026-09-13 17:17:08 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -52,18 +52,9 @@
 
           src = ./.;
 
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            outputHashes = {
-              "collections-0.1.0" = "sha256-BuY0YCmTlkGqrDLjOTatzWa/VoOo4kQtPxL7QkwjWTA=";
-              "gpui-component-0.5.2" = "sha256-BOUSwFpSECRvsNi+F5psCO4mvgyF7SAbJ01r6RyH3A8=";
-              "zed-font-kit-0.14.1-zed" = "sha256-KXygi0olNQi5yM8eaJVykNDtbPMDjT+cWPBF8UrtXR4=";
-              "zed-reqwest-0.12.15-zed" = "sha256-p4SiUrOrbTlk/3bBrzN/mq/t+1Gzy2ot4nso6w6S+F8=";
-              "zed-scap-0.0.8-zed" = "sha256-BihiQHlal/eRsktyf0GI3aSWsUCW7WcICMsC2Xvb7kw=";
-              "zed-xim-0.4.0-zed" = "sha256-pRT4Sz1JU9ros47/7pmIW9kosWOGMOItcnNd+VrvnpE=";
-              "wasm_thread-0.3.3" = "sha256-+lRLCIk0S6Y5ORYjDKsYYHia2FtoSoh+rWkQh7mnPBE=";
-            };
-          };
+          # Vendored with fetchCargoVendor, which pulls crates from static.crates.io;
+          # the crates.io API endpoint rejects curl's default user agent.
+          cargoHash = "sha256-4Ok9oUgrBBxpZsvvwasY6Z4C7qDrNVQdrQwdMYL7faU=";
 
           nativeBuildInputs =
             [ pkgs.makeWrapper ]
@@ -72,7 +63,7 @@
           buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux linuxLibs;
 
           preBuild = ''
-            ln -sfn gpui-component-assets-0.5.1 "$NIX_BUILD_TOP/cargo-vendor-dir/assets"
+            ln -sfn gpui-component-assets-0.5.1 "$NIX_BUILD_TOP/$(stripHash "$cargoDeps")/assets"
           '';
 
           doCheck = false;
